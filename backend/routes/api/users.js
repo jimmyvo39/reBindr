ad4b6e279f2a0b04b012c4eb0a28b86b1ba8aeca
Justin Kilburn <ThatMoonMan@gmail.com>
2022-12-19 10:57:14 -0500

    backend user auth complete

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -48,7 +48,7 @@ router.post('/register', validateRegisterInput, async (req, res, next) => {
   });
 })
 
-router.post('/login', validateRegisterInput, async (req, res, next) => {
+router.post('/login', validateLoginInput, async (req, res, next) => {
   passport.authenticate('local', async function(err, user) {
     if (err) return next(err);
     if (!user) {
