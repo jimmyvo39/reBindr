6a0968a1b960357570b83c866a96c0ece2f19936
Justin Kilburn <ThatMoonMan@gmail.com>
2022-12-19 10:40:06 -0500

    session token

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
-    isProduction: process.env.NODE_ENV === 'production'
+    isProduction: process.env.NODE_ENV === 'production',
+    secretOrKey: process.env.SECRET_OR_KEY,
   }
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,13 +4,8 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const passport = require('passport');
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({
-    message: "GET /api/users"
-  });
-});
+const { loginUser, restoreUser } = require('../../config/passport');
+const { isProduction } = require('../../config/keys');
 
 router.post('/register', async (req, res, next) => {
   const user = await User.findOne({
@@ -42,7 +37,7 @@ router.post('/register', async (req, res, next) => {
       try {
         newUser.hashedPassword = hashedPassword;
         const user = await newUser.save();
-        return res.json({ user });
+        return res.json(await loginUser(user));
       }
       catch(err) {
         next(err);
@@ -60,8 +55,24 @@ router.post('/login', async (req, res, next) => {
       err.errors = { email: "Invalid credentials" };
       return next(err);
     }
-    return res.json({ user });
+    return res.json(await loginUser(user));
   })(req, res, next);
+});
+
+router.get('/current', restoreUser, (req, res) => {
+  if (!isProduction) {
+    // In development, allow React server to gain access to the CSRF token
+    // whenever the current user information is first loaded into the
+    // React application
+    const csrfToken = req.csrfToken();
+    res.cookie("CSRF-TOKEN", csrfToken);
+  }
+  if (!req.user) return res.json(null);
+  res.json({
+    _id: req.user._id,
+    username: req.user.username,
+    email: req.user.email
+  });
 });
 
 module.exports = router;
