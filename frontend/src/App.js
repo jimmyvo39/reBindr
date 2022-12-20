af088d8540c58250633f80bfc939ef688e16bbb8
Ming Zhang <mzhang630@gmail.com>
2022-12-20 10:58:28 -0500

    fixed user login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,17 @@ import NavBar from "./components/NavBar/NavBar";
 import BigCalendar from "./components/Calendar";
 import LoginForm from "./components/LoginForm/LoginForm";
 import SignUpForm from "./components/SignupForm/SignupForm";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { getCurrentUser } from "./store/session";
 
 function App() {
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCurrentUser()).then(() => setLoaded(true));
+  }, [dispatch]);
+
   const sessionUser = useSelector((state) => state.session.user);
 
   let SessionLinks;
@@ -20,26 +28,28 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <NavBar />
-        <Switch>
-          <Route exact path={"/"}>
-            {SessionLinks}
-          </Route>
-          <Route exact path={"/signup"}>
-            <SignUpForm />
-          </Route>
+    loaded && (
+      <BrowserRouter>
+        <div className="App">
+          <NavBar />
+          <Switch>
+            <Route exact path={"/"}>
+              {SessionLinks}
+            </Route>
+            <Route exact path={"/signup"}>
+              <SignUpForm />
+            </Route>
 
-          <Route path={"/test"}>
-            <>
-              <h1>test page</h1>
-              <BigCalendar />
-            </>
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+            <Route path={"/test"}>
+              <>
+                <h1>test page</h1>
+                <BigCalendar />
+              </>
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    )
   );
 }
 
