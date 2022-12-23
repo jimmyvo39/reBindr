a6c9d2593543d6c0ae7cbd501d5944628398f6ee
jimmyvo39 <84362272+jimmyvo39@users.noreply.github.com>
2022-12-23 00:51:16 -0500

    add scroll and correct modal style

--- frontend/src/components/ReminderShareForm/ReminderShareForm.js
+++ frontend/src/components/ReminderShareForm/ReminderShareForm.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 // import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import jwtFetch from "../../store/jwt";
+import '../InventoryCreateFormModal/InventoryCreateForm.css'
 
 const ReminderShareForm = (props) => {
     // const {id} = useParams();
@@ -24,26 +25,41 @@ const ReminderShareForm = (props) => {
     
     const [email, onEmailChange] = useInput();
     const [phone, onPhoneChange] = useInput();
+    const [item, setItem] = useState(true);
+    const setShowModal = props.setShowModal;
+
+    const closeModal = (e) => {
+        e.preventDefault();
+        setShowModal(false);
+        setItem(false);
+      };
 
     return (
         <>
-        <h1>Send Reminder</h1>
-        <form onSubmit={onSubmit}>
-        <input
-          type="email"
-          placeholder="email"
-          value={email}
-          onChange={onEmailChange}
-        />
-        <input
-          type="tel"
-          value={phone}
-          placeholder="phone"
-          onChange={onPhoneChange}
-        />
-        <button type="submit">Send reminder</button>
+        <div className="form-container ">
+            <div  className="buttonBox">
+                <button onClick={closeModal} className="xButton">
+                    <i className="fa-solid fa-x"></i>
+                </button>
+            </div>
+            <h1>Send Reminder</h1>
+            <form onSubmit={onSubmit} className="reminder-form">
+            <input
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={onEmailChange}
+            />
+            <input
+            type="tel"
+            value={phone}
+            placeholder="Phone"
+            onChange={onPhoneChange}
+            />
+            <button type="submit" className="create-form-btn">Send reminder</button>
 
-        </form>
+            </form>
+        </div>
         </>
     )
 }
