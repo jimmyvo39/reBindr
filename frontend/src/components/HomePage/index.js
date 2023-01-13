96039decfb43740e572ee75628e905cde7aeb2b7
Justin Kilburn <ThatMoonMan@gmail.com>
2023-01-13 11:37:56 -0500

    index bug fix

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./HomePage.css";
 import BigCalendar from "../Calendar";
 import InventoryIndex from "../InventoryIndex";
+import { getCurrentUser } from "../../store/session";
+import { useDispatch, useSelector } from "react-redux";
 
 const HomePage = () => {
+  const dispatch = useDispatch()
+  const currentUser = useSelector(state => state.session.user)
+
+  useEffect(() => {
+    dispatch(getCurrentUser())
+  },[])
+
   return (
     <>
+    {currentUser &&
       <div className="home-page">
         <InventoryIndex />
         <BigCalendar />
       </div>
+    }
     </>
   );
 };
