b1d076bc745c997db82d323111b26feb8c13c2d6
Justin Kilburn <ThatMoonMan@gmail.com>
2023-01-17 17:10:42 -0500

    datetime sucks

--- frontend/src/components/ReminderCreateForm/ReminderCreateForm.js
+++ frontend/src/components/ReminderCreateForm/ReminderCreateForm.js
@@ -7,6 +7,7 @@ import { useParams } from "react-router-dom";
 import { getInventory } from "../../store/inventories";
 // import { getCurrentUser } from "../../store/session";
 import './ReminderCreateForm.css'
+import moment, { min } from 'moment'
 
 function ReminderCreateForm(props) {
   const dispatch = useDispatch();
@@ -26,14 +27,47 @@ function ReminderCreateForm(props) {
     return [value, onChange];
   }
 
-  const [title, onTitleChange] = useInput("");
-  const [date, onDateChange] = useInput(new Date());
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState('');
   const [repeat, onRepeatChange] = useInput("");
 
+  const [showTitleError, setShowTitleError] = useState(false)
+  const titleError = 'Reminder title must be between 5 and 50 characters.'
+  const [showDateError, setShowDateError] = useState(false)
+  const dateError = 'Reminder must be in the future.'
+
+  const twentyMinutesLater = new Date();
+  twentyMinutesLater.setMinutes(twentyMinutesLater.getMinutes() + 20)
+  twentyMinutesLater.setHours(twentyMinutesLater.getHours() - 5);
+  const minimum = twentyMinutesLater.toISOString().split(':').slice(0,2).join(':')
 
   const onSubmit = (e) => {
     e.preventDefault();
-    const data = { uploader, item, title, date, repeat };
+
+    let newTwentyMinutesLater
+    if (!date) {
+      newTwentyMinutesLater = new Date();
+      newTwentyMinutesLater.setMinutes(newTwentyMinutesLater.getMinutes() + 20);
+    }
+    const data = { uploader, item, title, date: date ? date : newTwentyMinutesLater, repeat };
+    
+    const now = moment(new Date()).format("MMMM Do YYYY, h:mm a")
+    const inputDate = moment(date).format("MMMM Do YYYY, h:mm a")
+    setShowTitleError(false)
+    setShowDateError(false)
+    
+    if ((title.length < 5 || title.length > 50) && (date && inputDate < now)) {
+      setShowTitleError(true)
+      setShowDateError(true)
+      return
+    } else if (title.length < 5 || title.length > 50) {
+      setShowTitleError(true)
+      return
+    } else if (date && inputDate < now) {
+      setShowDateError(true)
+      return
+    }
+
     dispatch(createReminder(data));
     setShowModal(false);
     // setItem(false);
@@ -61,18 +95,25 @@ function ReminderCreateForm(props) {
           </button>
         </div>
         <h1>Create a reminder</h1>
-        <form className="reminder-form" form onSubmit={onSubmit}>
+        <form className="reminder-form" onSubmit={onSubmit}>
           <input
             type="text"
             placeholder="title"
             value={title}
-            onChange={onTitleChange}
+            onChange={(e) => setTitle(e.target.value)}
           />
+          {showTitleError &&
+            <div className="errors">{titleError}</div>
+          }
           <input
             type="datetime-local"
+            min={minimum}
             value={date}
-            onChange={onDateChange}
+            onChange={(e) => setDate(e.target.value)}
           />
+          {showDateError && 
+            <div className="errors">{dateError}</div>
+          }
           <button type="submit"  className="create-form-btn">Add reminder</button>
         </form>
 
